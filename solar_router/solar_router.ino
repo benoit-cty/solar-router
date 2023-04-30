d0a7f9769bc1c7785dc03715e202748c7192288a
benoit-cty <4-benoit-cty@users.noreply.git.leximpact.dev>
2023-04-30 19:06:21 +0200

    wip: mqtt

--- solar_router/solar_router.ino
+++ solar_router/solar_router.ino
@@ -36,7 +36,7 @@ float routagePuissance = -20; /* puissance d'injection réseau en watts : pour p
 int ajustePuissance = 0;      /* réglage puissance */
 float puissanceRoutage = 0;
 float mini_puissance = 0;
-float max_puissance = 3000;
+float max_puissance = 40;  // Set to the maximum power of your device. 2 000 for a boiler. 40 for a light bulb.
 float pas_dimmer1 = 1;
 float pas_dimmer2 = 0.1;
 float pas_dimmer3 = 5;
@@ -59,19 +59,23 @@ const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
 AsyncWebServer server(80);
 ESPDash dashboard(&server);
+
 Card button(&dashboard, BUTTON_CARD, "Auto");
 Card slider(&dashboard, SLIDER_CARD, "Puissance sortie 1 en %", "", 0, 95);
+Card voltage(&dashboard, GENERIC_CARD, "Voltage", "V");
+Card power2(&dashboard, GENERIC_CARD, "Power2", "W");
+Card frequency(&dashboard, GENERIC_CARD, "frequency", "Hz");
 Card consommationsurplus(&dashboard, GENERIC_CARD, "surplus / consommation", "watts");
 Card puissance(&dashboard, GENERIC_CARD, "puissance envoyée au ballon", "watts");
+Card intensite1(&dashboard, GENERIC_CARD, "Intensité 1", "A");
+Card intensite2(&dashboard, GENERIC_CARD, "Intensité 2", "A");
 Card energy1(&dashboard, GENERIC_CARD, "énergie sauvée", "kwh");
+Card energy2(&dashboard, GENERIC_CARD, "énergie 2", "kwh");
 Card valdim1(&dashboard, PROGRESS_CARD, "sortie 1", "%", 0, 95);
 Card valdim2(&dashboard, PROGRESS_CARD, "sortie 2", "%", 0, 95);
 
 ////////////// Fin connexion wifi //////////
 
-
-TaskHandle_t Task1;
-TaskHandle_t Task2;
 
 /* Broches utilisées */
 const int zeroCrossPin = 35; /* broche utilisée pour le zéro crossing */
@@ -83,101 +87,61 @@ const int Relay1 = 13;
 dimmerLamp dimmer1(pulsePin1, zeroCrossPin);
 dimmerLamp dimmer2(pulsePin2, zeroCrossPin);
 
-/* MQTT with Home Assistant
- * From https://github.com/dawidchyrzynski/arduino-home-assistant/blob/main/examples/hvac/hvac.ino
- */
+///////////// MQTT with Home Assistant /////////////
 WiFiClient client;
 // Unique ID must be set for MQTT
 HADevice device(MQTT_TOPIC_NAME);
 HAMqtt mqtt(client, device);
-/* MQTT Devices */
-// Mode Auto ON/OFF
-
+//// Setting MQTT Devices
+// Voltage = ByteData[1] * 0.0001;      // Tension
+HASensorNumber ha_voltage("voltage", HASensorNumber::PrecisionP1);
+// Intensite1 = ByteData[2] * 0.0001;   // Intensité 1
+HASensorNumber ha_current1("current1", HASensorNumber::PrecisionP1);
 // Power 1 in %
-HASensorNumber power1("Power output 1 in %", HASensorNumber::PrecisionP1);
-// PV Usage in W
-HASensorNumber production("surplus / consommation", HASensorNumber::PrecisionP1);
-// Power send to boiler in W
-HASensorNumber power_boiler("puissance envoyée au ballon", HASensorNumber::PrecisionP1);
+HASensorNumber ha_power1("power1", HASensorNumber::PrecisionP1);
+// Energy1 = ByteData[4] * 0.0001;      // Energie 1
+HASensorNumber ha_energy1("energy1", HASensorNumber::PrecisionP1);
+// ???
+HASensorNumber ha_production("ha_production", HASensorNumber::PrecisionP1);
+// Frequency = ByteData[8] * 0.01;      // Fréquence
+HASensorNumber ha_frequency("frequency", HASensorNumber::PrecisionP1);
+// Intensite2 = ByteData[10] * 0.0001;  // Intensité 2
+HASensorNumber ha_current2("current2", HASensorNumber::PrecisionP1);
+// Power2
+HASensorNumber ha_power2("power2", HASensorNumber::PrecisionP1);
+//
+HASensorNumber ha_energy2("energy2", HASensorNumber::PrecisionP1);
 // Energy saved
-HASensorNumber energy_saved("Energy saved", HASensorNumber::PrecisionP1);
+HASensorNumber ha_energy_saved("energy_saved", HASensorNumber::PrecisionP1);
 // Dimmer 1 output in %
-HASensorNumber dimmer_output("Dimmer output", HASensorNumber::PrecisionP1);
+HASensorNumber ha_dimmer1_output("dimmer1", HASensorNumber::PrecisionP1);
+HASensorNumber ha_dimmer2_output("dimmer2", HASensorNumber::PrecisionP1);
+// Mode Auto ON/OFF
+HASensorNumber ha_mode("mode_auto", HASensorNumber::PrecisionP0);
 
-
-// By default HAHVAC supports only reporting of the temperature.
-// You can enable feature you need using the second argument of the constructor.
-// Please check the documentation of the HAHVAC class.
-// HAHVAC hvac(
-//   "boiler",
-//   HAHVAC::TargetTemperatureFeature | HAHVAC::PowerFeature | HAHVAC::ModesFeature
-// );
 
 unsigned long lastTempPublishAt = 0;
 float lastTemp = 0;
 
-// void onTargetTemperatureCommand(HANumeric temperature, HAHVAC* sender) {
-//     float temperatureFloat = temperature.toFloat();
+// Multi-core
+TaskHandle_t Task1;
+TaskHandle_t Task2;
 
-//     Serial.print("Target temperature: ");
-//     Serial.println(temperatureFloat);
-
-//     sender->setTargetTemperature(temperature); // report target temperature back to the HA panel
-// }
-
-// void onPowerCommand(bool state, HAHVAC* sender) {
-//   if (state) {
-//     Serial.println("Power on");
-//   } else {
-//     Serial.println("Power off");
-//   }
-// }
-
-
-// void onModeCommand(HAHVAC::Mode mode, HAHVAC* sender) {
-//     Serial.print("Mode: ");
-//     if (mode == HAHVAC::OffMode) {
-//         Serial.println("off");
-//     } else if (mode == HAHVAC::AutoMode) {
-//         Serial.println("auto");
-//     } else if (mode == HAHVAC::CoolMode) {
-//         Serial.println("cool");
-//     } else if (mode == HAHVAC::HeatMode) {
-//         Serial.println("heat");
-//     } else if (mode == HAHVAC::DryMode) {
-//         Serial.println("dry");
-//     } else if (mode == HAHVAC::FanOnlyMode) {
-//         Serial.println("fan only");
-//     }
-
-//     sender->setMode(mode); // report mode back to the HA panel
-// }
-
+/////////////////// S E T U P /////////////////////////////
 void setup() {
-
   Serial.begin(115200);
-  // Target : 38400
-  // Default : 4800
-  Serial2.begin(4800, SERIAL_8N1, RXD2, TXD2);  //PORT DE CONNEXION AVEC LE CAPTEUR JSY-MK-194
+  Serial2.begin(38400, SERIAL_8N1, RXD2, TXD2);  //PORT DE CONNEXION AVEC LE CAPTEUR JSY-MK-194
   delay(300);
   u8g2.begin();            // ECRAN OLED
   u8g2.enableUTF8Print();  //nécessaire pour écrire des caractères accentués
   dimmer1.begin(NORMAL_MODE, ON);
   pinMode(Relay1, OUTPUT);
   digitalWrite(Relay1, LOW);
-
-
-
-
+  // WiFi connection
   WiFi.mode(WIFI_STA);  //Optional
   WiFi.begin(ssid, password);
   server.begin();
-
-
-
   delay(100);
-
-
   // set device's details (optional)
   device.setName("Solar PV router");
   device.setSoftwareVersion("1.0.0");
@@ -187,40 +151,11 @@ void setup() {
   device.enableLastWill();
 
   /* MQTT Devices */
-  // Power 1 in %
-  // power1.setIcon("mdi:home");
-  //   power1.setName("Analog voltage");
-  //   power1.setUnitOfMeasurement("V");
-  // PV Usage in W
-  // HASensorNumber production("surplus / consommation", HASensorNumber::PrecisionP1);
-  // Power send to boiler in W
-  //HASensorNumber power_boiler("puissance envoyée au ballon", HASensorNumber::PrecisionP1);
-  // Energy saved
-  //HASensorNumber energy_saved("Energy saved", HASensorNumber::PrecisionP1);
-  energy_saved.setIcon("mdi:home");
-  energy_saved.setName("Energy saved");
-  energy_saved.setUnitOfMeasurement("kWh");
-  // Dimmer 1 output in %
-  //HASensorNumber dimmer_output("Dimmer output", HASensorNumber::PrecisionP1);
+  // TODO: customize all Home Assistant integration
+  ha_power1.setIcon("mdi:home");
+  ha_power1.setName("Power1");
+  ha_power1.setUnitOfMeasurement("W");
 
-
-
-  // assign callbacks (optional)
-  // hvac.onTargetTemperatureCommand(onTargetTemperatureCommand);
-  // hvac.onPowerCommand(onPowerCommand);
-  // hvac.onModeCommand(onModeCommand);
-
-  // configure HVAC (optional)
-  // hvac.setName("My HVAC");
-  // hvac.setMinTemp(10);
-  // hvac.setMaxTemp(30);
-  // hvac.setTempStep(0.5);
-
-  // You can set retain flag for the HA commands
-  // hvac.setRetain(true);
-
-  // You can choose which modes should be available in the HA panel
-  // hvac.setModes(HAHVAC::OffMode | HAHVAC::CoolMode);
   mqtt.begin(MQTT_BROKER_IP, MQTT_USER, MQTT_PASSWORD);
 
   //Code pour créer un Task Core 0//
@@ -244,45 +179,52 @@ void setup() {
     &Task2,                 /* Task handle to keep track of created task */
     1);                     /* pin task to core 1 */
   delay(500);
+  Serial.println("Setup done.");
 }
 
 //programme utilisant le Core 1 de l'ESP32//
-
-
-void Datas() {
-
+void ReadPowerMeter() {
   delay(60);
-
+  // Modbus RTU message to get the data
   byte msg[] = { 0x01, 0x03, 0x00, 0x48, 0x00, 0x0E, 0x44, 0x18 };
-
   int i;
   int len = 8;
 
-  ////// Envoie des requêtes Modbus RTU sur le Serial port 2
+  // Send message
   for (i = 0; i < len; i++) {
     Serial2.write(msg[i]);
   }
-  len = 0;
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 
   ////////// Reception  des données Modbus RTU venant du capteur JSY-MK-194 ////////////////////////
-
-
   int a = 0;
   while (Serial2.available()) {
     ByteArray[a] = Serial2.read();
     a++;
   }
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////////
+  //// Sanity Checks
+  if (ByteArray[0] != 0x01) {
+    Serial.print("ERROR Message received do not start with 0x01: ");
+    Serial.println(a);
+    for (i = 0; i < a; i++) {
+      Serial.print(ByteArray[i], HEX);
+      Serial.print("-");
+    }
+    Serial.println(".");
+  }
 
-
-  //////// Conversion HEX /////////////////
-
-
+  if (a != 61) {  // 61
+    Serial.print("ERROR Message received have wrong length: ");
+    Serial.println(a);
+    for (i = 0; i < a; i++) {
+      Serial.print(i);
+      Serial.print("=");
+      Serial.print(ByteArray[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println(".");
+  }
+  //////// Extract response /////////////////
   ByteData[1] = ByteArray[3] * 16777216 + ByteArray[4] * 65536 + ByteArray[5] * 256 + ByteArray[6];       // Tension en Volts
   ByteData[2] = ByteArray[7] * 16777216 + ByteArray[8] * 65536 + ByteArray[9] * 256 + ByteArray[10];      // Intensité 1 en Ampères
   ByteData[3] = ByteArray[11] * 16777216 + ByteArray[12] * 65536 + ByteArray[13] * 256 + ByteArray[14];   // Puissance 1 en Watts
@@ -294,21 +236,7 @@ void Datas() {
   ByteData[11] = ByteArray[43] * 16777216 + ByteArray[44] * 65536 + ByteArray[45] * 256 + ByteArray[46];  // Puissance 2 en Watts
   ByteData[12] = ByteArray[47] * 16777216 + ByteArray[48] * 65536 + ByteArray[49] * 256 + ByteArray[50];  // Energie 2 en kwh
 
-  if (a != 50) {
-    Serial.print("ERROR Message received have wrong length: ");
-    Serial.println(a);
-    for (i = 0; i < a; i++) {
-      Serial.print(ByteArray[a], HEX);
-      Serial.print("-");
-    }
-    Serial.println(".");
-  }
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
   ///////// Normalisation des valeurs ///////////////
-
   Voltage = ByteData[1] * 0.0001;      // Tension
   Intensite1 = ByteData[2] * 0.0001;   // Intensité 1
   Power1 = ByteData[3] * 0.0001;       // Puissance 1
@@ -322,13 +250,22 @@ void Datas() {
 
   if (Sens2 == 1) {
     ajustePuissance = -Power2;
-  }
-
-  if (Sens2 == 0) {
+  } else {
     ajustePuissance = Power2;
   }
 
+  // Inversion des mesures
+  // float Intensite_tmp = Intensite1;
+  // int Power_tmp = Power1;
+  // int Energy_tmp = Energy1;
 
+  // float Intensite1 = Intensite2;
+  // int Power1 = Power2;
+  // int Energy1 = Energy2;
+
+  // Intensite2 = Intensite_tmp;
+  // Power2 = Power_tmp;
+  // Energy2 = Energy_tmp;
   ////////////////////////////////////////////////////////////////////////////////////////////////////
 }
 
@@ -336,8 +273,6 @@ void Datas() {
 
 void Task1PowerMonitoring(void* pvParameters) {
   for (;;) {
-
-
 
     button.attachCallback([&](bool value) {
       Auto = value;
@@ -353,51 +288,46 @@ void Task1PowerMonitoring(void* pvParameters) {
 
 
     if (Auto == 0) {
-      Datas();
+      ReadPowerMeter();
       valDim1 = Value;
-      dimmer1.setState(ON);
-      delay(100);
       dimmer1.setPower(Value);
+      delay(100);
+      dimmer1.setState(ON);
     }
 
     if (Auto == 1) {
-      Datas();
-
-
+      ReadPowerMeter();
       // calcul triacs ///
       // réglages Dimmer 1 ///
-
       if (valDim1 < 1) {
         dimmer1.setState(OFF);
       }
 
       if (Power2 == 0 && Power1 == 0) {
         valDim1 = 0;
-        dimmer1.setPower(valDim1);
         dimmer1.setState(OFF);
+        dimmer1.setPower(valDim1);
       }
 
       if (ajustePuissance <= -400 && valDim1 < 90) {
         valDim1 = valDim1 + pas_dimmer3;
-        dimmer1.setState(ON);
-        delay(200);
         dimmer1.setPower(valDim1);
+        delay(200);
+        dimmer1.setState(ON);
       }
       if (ajustePuissance > -400 && ajustePuissance <= -150 && valDim1 < 94) {
         valDim1 = valDim1 + pas_dimmer1;
-        dimmer1.setState(ON);
-        delay(200);
         dimmer1.setPower(valDim1);
+        delay(200);
+        dimmer1.setState(ON);
       }
-
 
       if (ajustePuissance <= routagePuissance && ajustePuissance > -150 && valDim1 < 94.99) {
         valDim1 = valDim1 + pas_dimmer2;
-        dimmer1.setState(ON);
-        delay(200);
         dimmer1.setPower(valDim1);
+        delay(200);
+        dimmer1.setState(ON);
       }
-
 
       if (ajustePuissance > routagePuissance && ajustePuissance <= 30 && valDim1 > 0.01) {
         valDim1 = valDim1 - pas_dimmer2;
@@ -483,17 +413,10 @@ void Task1PowerMonitoring(void* pvParameters) {
         dimmer2.setState(OFF);
       }
 
-
-
-
-
-      // réglage relay //
-
+      //////////////////// Set relay state ///////////////
       if (Power1 > relayOn) {
         digitalWrite(Relay1, HIGH);
       }
-
-
       if (Power1 < relayOff) {
         digitalWrite(Relay1, LOW);
       }
@@ -514,8 +437,14 @@ void Task2ExternalInterface(void* pvParameters) {
     // affichage page web //
 
     consommationsurplus.update(ajustePuissance);
+    voltage.update(Voltage);
+    frequency.update(Frequency);
+    intensite1.update(Intensite1);
+    intensite2.update(Intensite2);
     puissance.update(Power1);
+    power2.update(Power2);
     energy1.update(Energy1);
+    energy2.update(Energy2);
     valdim1.update(valDim1);
     valdim2.update(valDim2);
     button.update(Auto);
@@ -570,10 +499,37 @@ void Task2ExternalInterface(void* pvParameters) {
     u8g2.sendBuffer();  // l'image qu'on vient de construire est affichée à l'écran
 
     mqtt.loop();
-    // Send temp every 3 seconds
+    // Send values to Home Assistant every 3 seconds
     if ((millis() - lastTempPublishAt) > 3000) {
-      energy_saved.setValue(lastTemp);
-      // hvac.setCurrentTemperature(lastTemp);
+
+
+      /*
+
+    button.update(Auto);
+*/
+
+      ha_voltage.setValue(Voltage);
+      // Intensite1 = ByteData[2] * 0.0001;   // Intensité 1
+      ha_current1.setValue(Intensite1);
+      // Power 1 in %
+      ha_power1.setValue(Power1);
+      // Energy1 = ByteData[4] * 0.0001;      // Energie 1
+      ha_energy1.setValue(Energy1);
+      // ???
+      // ha_production.setValue(XXX);
+      // Frequency
+      ha_frequency.setValue(Frequency);
+      // Intensite2 = ByteData[10] * 0.0001;  // Intensité 2
+      ha_current2.setValue(Intensite2);
+      // Power2
+      ha_power2.setValue(Power2);
+      //
+      ha_energy2.setValue(Energy2);
+      // Energy saved
+      ha_energy_saved.setValue(ajustePuissance);
+      ha_dimmer1_output.setValue(valDim1);
+      ha_dimmer2_output.setValue(valDim2);
+      ha_mode.setValue(Auto);
       lastTempPublishAt = millis();
       lastTemp += 0.5;
     }
